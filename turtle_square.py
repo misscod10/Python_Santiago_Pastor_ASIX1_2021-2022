127b46aaf34a5b639924d775f6cfce02d3325b4a
Tu Nombre <cf19santiago.pastor@iesjoandaustria.org>
2021-09-29 17:11:55 +0200

    29/9/2021

---
+++ turtle_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+    turtle_square.py
+    Demostració de l'ús de Turtle dibuixant un quadrat
+"""
+
+import turtle
+tortuga = turtle.Pen()
+tortuga.color('red')
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.color('blue')
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.color('red')
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.color('blue')
+tortuga.forward(100)
+tortuga.left(135)
+tortuga.color('green')
+tortuga.forward(145)
+input()
