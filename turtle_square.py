f8f42483cdcabb8edb540a6e59dfabedc7bff405
Tu Nombre <cf19santiago.pastor@iesjoandaustria.org>
2021-09-29 18:15:19 +0200

    29/9/2021

--- turtle_square.py
+++ turtle_square.py
@@ -3,8 +3,8 @@
     turtle_square.py
     Demostració de l'ús de Turtle dibuixant un quadrat
 """
-
 import turtle
+import math
 tortuga = turtle.Pen()
 tortuga.color('red')
 tortuga.forward(100)
@@ -19,5 +19,5 @@ tortuga.color('blue')
 tortuga.forward(100)
 tortuga.left(135)
 tortuga.color('green')
-tortuga.forward(145)
+tortuga.forward(math.sqrt((100**2)+(100**2)))
 input()
