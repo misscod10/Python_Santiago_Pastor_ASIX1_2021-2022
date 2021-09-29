802b07e73f18ed34a4f74c3481fe1592ac4e66c6
Tu Nombre <cf19santiago.pastor@iesjoandaustria.org>
2021-09-29 18:26:01 +0200

    29/9/2021

--- turtle_square.py
+++ turtle_square.py
@@ -5,6 +5,7 @@
 """
 import turtle
 import math
+costat=100
 tortuga = turtle.Pen()
 tortuga.color('red')
 tortuga.forward(100)
@@ -19,5 +20,5 @@ tortuga.color('blue')
 tortuga.forward(100)
 tortuga.left(135)
 tortuga.color('green')
-tortuga.forward(math.sqrt((100**2)+(100**2)))
+tortuga.forward(math.sqrt((costat**2)+(costat**2)))
 input()
