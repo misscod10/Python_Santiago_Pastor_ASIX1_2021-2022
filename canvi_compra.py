6e2c7692211b1baf68393a4782566efe5850bb6b
Tu Nombre <cf19santiago.pastor@iesjoandaustria.org>
2021-10-13 18:49:38 +0200

    13/10/2021

---
+++ canvi_compra.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+Entregat=float(sys.argv[1])
+Total_Ticket=float(sys.argv[2])
+print("Entregat: ", Entregat)
+print("Total ticket: ", Total_Ticket)
+A_Retornar=Entregat-Total_Ticket
+Retorn=A_Retornar
+Billets_de_10=0
+Billets_de_5=0
+Moneda_de_1=0
+Moneda_de_05=0
+while A_Retornar>=0.5:
+    if A_Retornar >= 10.0:
+        Billets_de_10+=1
+        A_Retornar-=10
+        
+    elif A_Retornar >= 5.0:
+        Billets_de_5+=1
+        A_Retornar-=5
+        
+    elif A_Retornar >= 1.0:
+        Moneda_de_1+=1
+        A_Retornar-=1
+        
+    elif A_Retornar >= 0.5:
+        Moneda_de_05+=1
+        A_Retornar-=0.5
+print(    )
+if Billets_de_10 >=1:
+    print(Billets_de_10 ," x billet de 10€")
+if Billets_de_5 >=1:
+    print(Billets_de_5 ," x billet de 5€")
+if Moneda_de_1 >=1:
+    print(Moneda_de_1 ," x moneda de 1€")
+if Moneda_de_05 >=1:
+    print(Moneda_de_05 ," x moneda de 0.5€")
+
+print("---------------------------------")
+print("Total canvi ", Retorn,"€")
