f4195539b58965ef4b952c2d838f17c9ff98b488
Tu Nombre <cf19santiago.pastor@iesjoandaustria.org>
2021-10-15 16:45:38 +0200

    15/10/2021

--- edat_gos.py
+++ edat_gos.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #Aquest programa en python ens serveix per calcular l'edat d'un gos en anys humans.
 import sys
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
     print("     >>>   Has d'introduïr com a paràmetre l'edat del gos   <<<")
     print("     >>>   Ex: python3 edat_gos.py <anys del gos>           <<<")
     print("     >>>   Torna-ho a provar d'aquesta manera               <<<")
