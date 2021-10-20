f1dde65be3eff07fcf663153bde6d726a98bdda2
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2021-10-20 19:23:09 +0200

    20/10/2021

---
+++ color_posicio.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#Aquest programa ens permet saber si el cuadrat que li donem es blanc o negre.
+_author_="Santiago Pastor Serrano"
+import sys
+posició_escollida=sys.argv[1]
+columna=posició_escollida[0]
+fila=int(posició_escollida[1])
+if columna=="a":
+    color="negre"
+    color_contrari="blanc"
+elif columna=="b":
+    color="blanc"
+    color_contrari="negre"
+elif columna=="c":
+    color="negre"
+    color_contrari="blanc"
+elif columna=="d":
+    color="blanc"
+    color_contrari="negre"
+elif columna=="e":
+    color="negre"
+    color_contrari="blanc"
+elif columna=="f":
+    color="blanc"
+    color_contrari="negre"
+elif columna=="g":
+    color="negre"
+    color_contrari="blanc"
+elif columna=="h":
+    color="blanc"
+    color_contrari="negre"
+else:
+    print("Posició invàlida")
+    exit
+if (fila % 2) == 0:
+    print(color_contrari)
+else:
+    print(color)
