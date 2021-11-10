a467f5e853831dc4c0b19afcc91a2cc4c4adb189
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2021-11-10 18:45:32 +0100

    10/11/2021

--- Estructures/salt_de_granota.py
+++ Estructures/salt_de_granota.py
@@ -4,11 +4,21 @@ import doctest
 __author__="Santiago Pastor Serrano"
 
 def main (inici,final,salt):
+    """
+    >>> main(10,18,2)
+    ['10', '12', '14', '16','18']
+    >>> main(10,17,2)
+    ['10', '12', '14', '16']
+    >>> main(18,10,-2)
+    ['18', '16', '14', '12','10']
+    """
+    resultat=[]
     if salt == 0:
         raise ValueError('El valor de salt no pot ser 0')
         quit
     for index in range(inici,final,salt):
-        print(index)
+        resultat.append(str(index))
+    print(resultat)
 
 if __name__=='__main__':
-    main(sys.argv[1],sys.argv[2],sys.argv[3])
+    main(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
