0fea89c1e2d82d66e1430d9396812253bc431188
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2021-11-17 18:54:26 +0100

    17/11/2021

--- endevina.py
+++ endevina.py
@@ -16,21 +16,20 @@ def main(numero_secret):
     """
     es_endevinat = False
     suposicio_usuari = None
-    while True:
+    while es_endevinat==False:
         suposicio_usuari = int(input("Quin número entre 1 i 10 he pensat? "))
         if suposicio_usuari < numero_secret:
-            print("El número secret és més gran que ", suposicio_usuari, ", torna a intentar-ho!")
+            print(f"El número secret és més gran que {suposicio_usuari}, torna a intentar-ho!")
         elif suposicio_usuari > numero_secret:
-            print("El número secret és més petit que ", suposicio_usuari, ", torna a intentar-ho!")
+            print(f"El número secret és més petit que {suposicio_usuari}, torna a intentar-ho!")
         elif suposicio_usuari == "":
-            print("Sento que t'hagis donat per vençut tan ràpid, el número secret era el ", numero_secret)
-            quit
+            print(f"Sento que t'hagis donat per vençut tan ràpid, el número secret era el {numero_secret}")
+            es_endevinat=True
         else:
-            print("Felicitats, l'has encertat era el", numero_secret,"!!!")
-        # print(f"El número secret és més petit que {suposicio_usuari}, torna a intentar-ho!")
-        # print(f"El número secret és més gran que {suposicio_usuari}, torna a intentar-ho!")
-        # print(f"Felicitats, l'has encertat era el {numero_secret}!!!")
-        # print(f"Sento que t'hagis donat per vençut tan ràpid, el número secret era el {numero_secret}")
+            print(f"Felicitats, l'has encertat era el {numero_secret}!!!")
+            es_endevinat=True
+
+    
 
 if __name__ == "__main__":
     # Passem un nombre aleatori a la funció principal.
