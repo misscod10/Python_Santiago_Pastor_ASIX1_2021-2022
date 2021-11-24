ac13ef7dfd7cd689824e6415600c123802aee8f9
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2021-11-24 18:31:55 +0100

    24/11/21

---
+++ taula_de_multiplicar.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+
+import sys, os
+
+"""
+taula_de_multiplicar.py rep un nombre com a paràmetre i imprimeix la seva taula de multiplicar.
+"""
+__author__   = "Santiago Pastor Serrano"
+__email__    = "cf19santiago.pastor@iesjoandaustria.org"
+__license__  = "GPL V3"
+
+def main(nombre_protagonista):
+    """
+    Aquest programa rep un nombre com a paràmetre i retorna la seva taula de multiplicar en forma de cadena de text.
+    >>> print(main(5))
+    5 x 1 = 5
+    5 x 2 = 10
+    5 x 3 = 15
+    5 x 4 = 20
+    5 x 5 = 25
+    5 x 6 = 30
+    5 x 7 = 35
+    5 x 8 = 40
+    5 x 9 = 45
+    5 x 10 = 50
+    <BLANKLINE>
+    >>> print(main(-3))
+    -3 x 1 = -3
+    -3 x 2 = -6
+    -3 x 3 = -9
+    -3 x 4 = -12
+    -3 x 5 = -15
+    -3 x 6 = -18
+    -3 x 7 = -21
+    -3 x 8 = -24
+    -3 x 9 = -27
+    -3 x 10 = -30
+    <BLANKLINE>
+    """
+    taula_result = ""
+    for i in range(1,11):
+        taula_result+= f"{nombre_protagonista} x {i} = {nombre_protagonista*i}\n"
+    return taula_result
+
+if __name__ == "__main__":
+    main(sys.argv[1])
