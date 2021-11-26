2aee5ad1813a8d2dc43629e8394c08675d366fb6
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2021-11-26 16:05:32 +0100

    26/11/2021

---
+++ Estructures/Exercisis/barres_optimitzada.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+import random, os
+
+"""
+barres.py mostra gràficament els elements d'una llista 
+"""
+__author__   = "Santiago Pastor Serrano"
+__email__    = "cf19santiago.pastor@iesjoandaustria.org"
+__license__  = "GPL V3"
+
+def main(llista):
+    """
+    Visualitza gràficament els valors de llista
+    >>> main([1, 2, 3])
+    * 1
+    ** 2
+    *** 3
+    >>> main([3,1,3])
+    *** 3
+    * 1
+    *** 3
+    """
+    os.system('clear')
+    # El teu codi a partir d'aquí
+    for i in llista:
+        print("*" * i ,i)
+if __name__ == "__main__":
+    vector_valors = []
+    for index in range(10):
+        vector_valors.append(random.randrange(30) + 1)
+    main(vector_valors)
