faf169dcf2f931fa5e834495dc5e18d620dd67fc
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2022-01-28 16:17:26 +0100

    27/1/2022

---
+++ my_clipboard.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+import clipboard
+"""
+myclipboard.py es un archivo de prueba del modulo clipoboard en python.
+"""
+__author__   = "Santiago Pastor Serrano"
+__email__    = "cf19santiago.pastor@iesjoandaustria.org"
+__license__  = "GPL V3"
+print("|->" + clipboard.paste() + "<-|")
