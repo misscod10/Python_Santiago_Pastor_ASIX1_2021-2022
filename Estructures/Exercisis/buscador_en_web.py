7a1554dec00aacb105adbaa4fea0d043a13cbd45
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2022-02-04 17:09:49 +0100

    4/2/2022

---
+++ Estructures/Exercisis/buscador_en_web.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+__author__   = "Santiago Pastor Serrano"
+__email__    = "cf19santiago.pastor@iesjoandaustria.org"
+__license__  = "GPL V3"
+"""
+Aquest programa busca en una pàgina web una informàcio específica
+"""
+import requests
+from bs4 import BeautifulSoup
+def main():
+    url= "https://es.wikipedia.org/wiki/Wikipedia:Portada"
+    web=(requests.get(url))
+    if web.ok:
+        sopa_web= BeautifulSoup(web.text, "html.parser")
+        Articulo_destacado=(sopa_web.find(id="Artículo_destacado")).parent
+        Articulo_bueno=(sopa_web.find(id="Artículo_bueno")).parent
+        print((Articulo_destacado.find(class_="mw-headline")).text)
+        print((Articulo_bueno.find(class_="mw-headline")).text)
+    else:
+        print("Alguna cosa ha anat malament llegint la web." + str(web.status_code) + ":" + web.reason)
+if __name__ == "__main__":
+    main()
