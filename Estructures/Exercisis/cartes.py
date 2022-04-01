6f9056af8a2e675968bf4a408aafe762ca0324c4
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2022-04-01 17:21:37 +0200

    1/4/2022

--- Estructures/Exercisis/cartes.py
+++ Estructures/Exercisis/cartes.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 import random
 __author__="Santiago Pastor Serrano"
-
+COLUMNAS=13
 def main():
     simbolos={"cor":"\U00002665","diamant":"\U000025C6","pica":"\U00002660","trevol":"\U00002663"}
     ma=[]
-    pal="cor"
+    pal=["cor","diamant","pica","trevol"]
     n="Q"
     pq = [ 
     " ___ ", 
@@ -35,14 +35,18 @@ def main():
     "| " + simbolos["diamant"] + " |", 
     "|__5|" 
     ]
-    carta_generada=genera_carta(n,pal,simbolos) 
+    carta_generada=genera_carta(n,pal[1],simbolos) 
     ma=[cd,ut,As,pq,carta_generada]
     imprimir_mano(ma)
+    baralla=genera_baralla(pal,simbolos)
+    imprimir_mano(baralla)
 
 def imprimir_mano(ma):
+    if len(ma)>COLUMNAS:
+        imprimir_mano(ma[:-COLUMNAS])
     for indice in range(4):
         linea=""
-        for carta in ma:
+        for carta in ma[-COLUMNAS:]:
             linea+= carta[indice]
         print(linea)
 
@@ -50,13 +54,25 @@ def imprimir_mano(ma):
 def genera_carta(n, pal, simbolos):
     carta_generada=[ 
     " ___ ", 
-    "|"+ n.ljust(3," ") +  "|", 
+    "|"+ str(n).ljust(3," ") +  "|", 
     "| " + simbolos[pal] + " |", 
-    "|"+ n.rjust(3,"_")+"|" 
-    ]  
-    for linea in carta_generada:
-        print(linea)
+    "|"+ str(n).rjust(3,"_")+"|" 
+    ] 
     return carta_generada
+
+
+def genera_baralla(pal,simbolos):
+    baralla=[]
+    for i in pal:
+        for x in range(2,11):
+            carta_generada=genera_carta(x,i,simbolos)
+            baralla.append(carta_generada)
+        for y in ("J","Q","K","A"):
+            carta_generada=genera_carta(y,i,simbolos)
+            baralla.append(carta_generada)
+    return baralla
+        
+    
 
 if __name__=="__main__":
     main()
