219621e755ad0418bc851e4b807531063baa05de
cf19santiago <cf19santiago.pastor@iesjoandaustria.org>
2022-04-08 17:29:23 +0200

    8/4/2022

--- Estructures/Exercisis/cartes.py
+++ Estructures/Exercisis/cartes.py
@@ -40,7 +40,9 @@ def main():
     imprimir_mano(ma)
     baralla=genera_baralla(pal,simbolos)
     imprimir_mano(baralla)
-
+    baralla=mezclar(baralla)
+    imprimir_mano(baralla)
+    
 def imprimir_mano(ma):
     if len(ma)>COLUMNAS:
         imprimir_mano(ma[:-COLUMNAS])
@@ -72,7 +74,46 @@ def genera_baralla(pal,simbolos):
             baralla.append(carta_generada)
     return baralla
         
-    
+def mezclar(baralla):
+    metodos={1:lambda x:randomizar(x), 2:lambda x:mitades(x), 3:lambda x:especial(x)}
+    metodo=int(input("Con que método quieres mezclar las cartas?\n\n1. Todo random\n2. Por mitades\n3. Especial\n"))
+    resultat=metodos.get(metodo, lambda x : None)(baralla)
+    return resultat
+
+
+def randomizar(baralla):
+     resultat=[] 
+     for i in range(len(baralla)):
+          resultat.append(baralla[i])
+     for i in range(len(resultat)):
+        origen=random.randint(0,len(resultat)-1)
+        destí=random.randint(0,len(resultat)-1)
+        resultat[origen], resultat[destí]= resultat[destí], resultat[origen]
+     return resultat
+
+
+def mitades(baralla):
+    resultat=[]
+    meitat1=[]
+    meitat2=[]
+    for i in range(0,len(baralla)//2):
+        meitat1.append(baralla[i])
+    for x in range(len(baralla)//2,len(baralla)):
+          meitat2.append(baralla[x])
+    meitat1=randomizar(meitat1)
+    meitat2=randomizar(meitat2)
+    for z in range(len(baralla)//2):
+        resultat.append(meitat1[z])
+        resultat.append(meitat2[z])
+    return resultat
+
+
+def especial(baralla):
+    resultat=baralla
+    for i in range(3):
+        paso1=mitades(resultat)
+        resultat=randomizar(paso1)
+    return resultat
 
 if __name__=="__main__":
     main()
